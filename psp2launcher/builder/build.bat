de0e529ac3f4bc8dcdf05745193b51316a75a508
rsn8887 <rsn8887@users.noreply.github.com>
2024-02-14 03:35:24 -0600

    bump version, update readme, update psp2 bubble builder

--- psp2launcher/builder/build.bat
+++ psp2launcher/builder/build.bat
@@ -1,6 +1,6 @@
-@set /p title="Insert bubble name: "
-@set /p rom="Insert rom name: "
+@set /p title="Insert bubble name (Example: Superfrog): "
+@set /p conf="Insert config name (Example: ux0:/data/uae4all/conf/superfrog.conf): "
 @set /p id="Insert bubble title ID (9 characters [NOTE: Only UPPERCASE letters or numbers]): "
-@echo|set /p="%rom%"> "assets/args.txt"
+@echo|set /p="%conf%"> "assets/args.txt"
 vita-mksfoex -s TITLE_ID=%id% "%title%" param.sfo
 vita-pack-vpk -s param.sfo -b eboot.bin "%title%.vpk" -a assets/icon0.png=sce_sys/icon0.png -a assets/bg.png=sce_sys/livearea/contents/bg.png -a assets/startup.png=sce_sys/livearea/contents/startup.png -a assets/template.xml=sce_sys/livearea/contents/template.xml -a assets/args.txt=args.txt
